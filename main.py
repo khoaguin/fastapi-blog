de5284650272b560f6650c0bc9e73e8289a22600
khoaguin <dkn.work@protonmail.com>
2023-01-12 17:55:20 +0700

    on doing blog

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": {"name": "DK"}}
+    return {'data': 'blog list'}
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get('/blog/unpublished')
+def unpublished():
+    # fetch all unpublished blogs
+    return {'data': 'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+

2db1284c594ead490a09d831f325dd5a4f8080f3
khoaguin <dkn.work@protonmail.com>
2023-01-13 20:54:19 +0700

    doing post, learning to debug FastAPI apps

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def index():
-    return {'data': 'blog list'}
+def index(limit, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} publised blogs from the databse'}
+    else:
+        return {'data': f'{limit} blogs from the databse'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -17,7 +23,18 @@ def show(id: int):
     return {'data': id}
 
 @app.get('/blog/{id}/comments')
-def comments(id: int):
+def comments(id: int, limit=10):
     # fetch comments of blog with id = id
     return {'data': {'1', '2'}}
 
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog post {blog.title} was created'}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
